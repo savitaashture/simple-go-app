322a2b8055e8d424828aa01d73dcf53441b50553
savitaashture <savita3020@gmail.com>
2019-06-03 12:57:58 +0530

    simple go app with sleep of 1 min

--- app.go
+++ app.go
@@ -8,11 +8,13 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("My first app")
 	fmt.Fprintf(w, "My first app with URL %s!", r.URL.Path[1:])
 	time.Sleep(60 * time.Second)
 }
 
 func main() {
+        fmt.Println("Its coming herre")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
